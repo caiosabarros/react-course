6950879cdcdab0bd2e96e543eb6ca1c7e4f640c9
Caio Sa <caiohenriquesa1@gmail.com>
2022-03-19 11:03:49 -0300

    stuck: why can I get the data?

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const UKRAINIAN_ADDRESS = "0x165CD37b4C644C2921454429E7F9358d18A45e14"
 function App() {
 
   const [balance, setBalance] = useState("");
+  const [transactions, setTransactions] = useState([]);
 
   async function getAddressBalance(_address){
     try {
@@ -24,7 +25,20 @@ function App() {
     }
   }
 
+  const getTransactionsList = async (_address, _quantity) => {
+    try {
+      const {data} = await axios.get(`https://api.etherscan.io/api?module=account&action=txlist&address=${_address}&startblock=0&endblock=99999999&page=1&offset=${_quantity}&sort=asc&apikey=${ETHERSCAN_KEY}`);
+      console.log(data);
+      //await Promise.all(data);
+      setTransactions(data.result);
+      console.log(transactions);
+    } catch(error){
+      console.log(error.message);
+    }
+  }
+
   useEffect(()=>{
+    getTransactionsList(UKRAINIAN_ADDRESS, 10)
     getAddressBalance(UKRAINIAN_ADDRESS);
     return ()=>{
     }
@@ -34,9 +48,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button
+        >
+          See Transactions
+        </button>
+
         <h1>Ukranian Balance on Ethereum Mainnet</h1>
         <h3>{(parseFloat(balance)/10**18).toFixed(3)} ETH</h3>
         {/*It is important to notice that when I click, I will call the function.
