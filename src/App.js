b6f94333d3c6fb8356f9d0d645e65d7bbbe45c78
Caio Sa <caiohenriquesa1@gmail.com>
2022-03-19 10:09:30 -0300

    lifecycle in functional tional components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react'
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  /* 
+  This is a functional simulation of a class component lifecycle.
+  */
+
+  /*
+  This useEffect below simulates a componentDidMount because of no-depedency.
+  It will run only once after the App() component is rendered.
+  */
+  useEffect(()=>{
+    setCounter(counter+1);
+    return ()=>{
+      setCounter(10);
+    }
+  },[])
+
+  const handleClick = () => {
+    setCounter(counter+1);
+  }
+
+  /*
+  This simulates a componentDidUpdate: there was an state update in the counter state.
+  So, this function will render, then also the App() component to load the new value of
+  counter on the screen.
+  */
+  useEffect(()=>{
+    console.log("updated");
+  },[counter])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +41,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>{counter}</h1>
+        {/*It is important to notice that when I click, I will call the function.
+        Passsing handleClick() would mean that I would run the result of the function 
+        when I click it. It the function returns a callback, then that may be 
+        interesting.*/}
+        <h1 onClick={handleClick}>Increment</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
